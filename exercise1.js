bf3380343cba11784539aa7768cc2c30fce2e051
sreyproek <chhornsreyproek@gmail.com>
2024-04-11 09:55:51 +0700

    move ex1-5 to js part3

--- exercise1.js
+++
@@ -1,9 +0,0 @@
-function squareArray(num) {
-    return num * num;
-  }
-  function call(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-      console.log(callback(array[index]));
-    }
-  }
-  call([1, 2, 3], squareArray); 
